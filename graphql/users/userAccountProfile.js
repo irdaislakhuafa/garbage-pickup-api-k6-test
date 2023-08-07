0a15c0c52b7dfad69544917e290b245b2d91e4b2
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-08-08 03:02:44 +0700

    added test for endpoints page "Profil Akun Pengguna"

--- graphql/users/userAccountProfile.js
+++ graphql/users/userAccountProfile.js
@@ -1,6 +1,26 @@
 import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 
-export default function () {
 
+const url = "http://localhost:8080/api/gql/graphql"
+const userId = '86ebd8d8-396e-43f8-a84b-7622720554df'
+
+export default function () {
+	const query = `
+		query {
+			user {
+				findById(id: "${userId}") {
+					name, address, phone, email
+				}
+			}
+		}
+	`
+	const body = JSON.stringify({ query: query })
+	const headers = {
+		'Content-Type': 'application/json'
+	}
+	const res = http.post(url, body, { headers: headers })
+	check(res, {
+		"get user is success": (r) => JSON.parse(r.body).errors == null
+	})
 };
