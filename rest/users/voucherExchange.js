8567c46dc2eb8baf21ba83efaf420cc693028a28
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2023-08-07 15:47:12 +0700

    added test for page "Penukaran Voucher"

---
+++ rest/users/voucherExchange.js
@@ -0,0 +1,31 @@
+import http from 'k6/http';
+import { check, group, sleep } from 'k6';
+
+const host = 'http://localhost:8080'
+const userId = '86ebd8d8-396e-43f8-a84b-7622720554df'
+
+export default function () {
+	let url = `${host}/api/rest/users/${userId}`
+	let headers = {
+		'Content-Type': 'application/json'
+	}
+
+	// get data user
+	let res = http.get(url)
+	check(res, {
+		"get user success": (r) => r.status === 200,
+	})
+
+	// get available voucher
+	url = `${host}/api/rest/userVouchers/vouchers`
+	let body = JSON.stringify({
+		userId: `${userId}`,
+		statuses: []
+	})
+
+	res = http.post(url, body, { headers: headers })
+	check(res, {
+		"get available user voucher success": (r) => r.status === 200,
+	})
+
+};
